1d638ec8c5a21be5ed9e386f15a42a2e35ba291d
Albert Cheung <albertcsm@gmail.com>
2022-03-07 23:34:09 +0800

    add favicon and basic pwa configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 
 module.exports = (env, options) => {
   return {
@@ -11,6 +12,22 @@ module.exports = (env, options) => {
     plugins: [
       new HtmlWebpackPlugin({
         title: '2D Craft',
+        favicon: 'src/assets/icon/favicon.png'
+      }),
+      new WebpackPwaManifest({
+        name: '2dcraft',
+        short_name: '2dcraft',
+        description: 'Minecraft like game in 2D',
+        background_color: '#ffffff',
+        icons: [
+          {
+            src: path.resolve('src/assets/icon/dirt.png'),
+            sizes: [36, 48, 72, 96, 144, 192, 120, 152, 167, 180],
+            ios: true
+          }
+        ],
+        ios: true,
+        publicPath: '.'  // https://github.com/arthurbergmz/webpack-pwa-manifest/issues/149
       }),
     ],
     module: {
